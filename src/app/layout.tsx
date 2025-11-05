0f754971ba8c9d7cbc841fd71ebf704e4777f55e
Aman Behera <amanbehera50@gmail.com>
2025-11-05 20:44:36 +0000

    Try fixing this error: `Runtime Error: Error: Failed to load chunk /_nex

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
-import { Toaster } from "@/components/ui/toaster";
-import { FirebaseClientProvider } from '@/firebase/client-provider';
+import { Providers } from './providers';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -22,10 +21,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@500;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <FirebaseClientProvider>
-          {children}
-        </FirebaseClientProvider>
-        <Toaster />
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
